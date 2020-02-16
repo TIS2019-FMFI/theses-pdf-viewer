7270133ac3037ffe07a5e2a96ccff71153a67faf
Matej <dobrakmato@gmail.com>
2020-02-16 23:58:36 +0100

    push built frontend

---
+++ frontend/build/precache-manifest.03e2920e7f9b0435c138ebefa4dc9e00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2044d5eaed1309bf4e03a86091d264c7",
+    "url": "/frontend/build/index.html"
+  },
+  {
+    "revision": "734ef897cee1da399707",
+    "url": "/frontend/build/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "d6c14f2886dc6c77fe2d",
+    "url": "/frontend/build/static/css/main.00dc1392.chunk.css"
+  },
+  {
+    "revision": "734ef897cee1da399707",
+    "url": "/frontend/build/static/js/2.99c7367e.chunk.js"
+  },
+  {
+    "revision": "d6c14f2886dc6c77fe2d",
+    "url": "/frontend/build/static/js/main.b091bded.chunk.js"
+  },
+  {
+    "revision": "df3b844a225d227ef103",
+    "url": "/frontend/build/static/js/runtime-main.f3c16588.js"
+  }
+]);
